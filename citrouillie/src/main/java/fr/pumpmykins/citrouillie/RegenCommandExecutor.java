1e9d7cbcb94cbba0647ea8e152da22127fe3671b
Sufike <l.christ54260@gmail.com>
2019-05-06 20:30:38 +0200

    need to fix the regen command if it works, need to simplify the code

--- citrouillie/src/main/java/fr/pumpmykins/citrouillie/RegenCommandExecutor.java
+++ citrouillie/src/main/java/fr/pumpmykins/citrouillie/RegenCommandExecutor.java
@@ -19,23 +19,76 @@ public class RegenCommandExecutor implements CommandExecutor {
 			
 			Player p = (Player) sender;
 			TNEAPI ecoAPI = TNE.instance().api();
-			if(p.getHealth() < p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue()) {
-				if(ecoAPI.canRemoveHoldings(sender.getName(),new BigDecimal(100))) {
+			double max_health = p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
+			if(p.getHealth() < max_health) {
+				
+				if(args.length < 1) {
 					
-					ecoAPI.removeHoldings(sender.getName(), new BigDecimal(100));
-					double health = p.getHealth();
-					double max_health = p.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
-					if(health+2 > max_health) {
-						p.setHealth(max_health);
-					} else {	
-						p.setHealth(health+2);
+					int cost = 0;
+					cost =(int) ((p.getHealth() / 2) *100);
+					
+					if(ecoAPI.canRemoveHoldings(sender.getName(),new BigDecimal(cost))) {
+						
+						ecoAPI.removeHoldings(sender.getName(), new BigDecimal(cost));
+						double health = p.getHealth();
+						if(health+2 > max_health) {
+							p.setHealth(max_health);
+						} else {	
+							p.setHealth(health+2);
+						}
+						p.sendMessage("1 Coeur vous à été redonné");
+						
+					} else {
+						
+						p.sendMessage("Vous n'avez pas l'argent necessaire !");
 					}
-					p.sendMessage("1 Coeur vous à été redonné");
-					
 				} else {
 					
-					p.sendMessage("Vous n'avez pas l'argent necessaire !");
+					if(args[0].matches("-?(0|[1-9]\\d*)")) {
+						
+						int cost = 0;
+						int regen = 0;
+						
+						for(int i = 0; i < Integer.parseInt(args[0]); i++) {
+							
+							regen+=2;
+							if(p.getHealth()+ regen > max_health) {
+								break;
+							}
+							double v = p.getHealth();
+							v = v / 2;
+							BigDecimal bd = new BigDecimal(v);
+							bd.setScale(0, BigDecimal.ROUND_FLOOR);
+							v = bd.doubleValue();
+							
+							cost = (int) (cost + ((1*v)/2 * 100));
+							
+						}
+						if(ecoAPI.canRemoveHoldings(sender.getName(),new BigDecimal(cost))) {
+							
+							ecoAPI.removeHoldings(sender.getName(), new BigDecimal(cost));
+							double health = p.getHealth();
+							if(health+regen > max_health) {
+								p.setHealth(max_health);
+							} else {	
+								p.setHealth(health+regen);
+							}
+							p.sendMessage(Integer.toString(regen/2)+" Coeur vous à été redonné");
+							
+						} else {
+							
+							p.sendMessage("Vous n'avez pas l'argent necessaire !");
+						}
+						
+						
+					} else {
+						
+						p.sendMessage("Veuillez indiquez un nombre de vie à regenerer !");
+					}
 				}
+			} else {
+				
+				p.sendMessage("Tu as déjà toute ta vie !");
 			}
 		}
 		return true;
